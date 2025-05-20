f49fb4cefb25e2b718f5f50e1b6557a27928dcf8
whiteskull20 <derekkchou@gmail.com>
2025-05-20 11:35:07 +0800

    modify agent

--- Q3/student_agent.py
+++ Q3/student_agent.py
@@ -3,12 +3,12 @@ import numpy as np
 from train import Agent as StudentAgent
 
 # Do not modify the input of the 'act' function and the '__init__' function. 
+s_agent = StudentAgent(67,21,None)
+s_agent.load('humanoid_sac')
 class Agent(object):
     """Agent that acts randomly."""
     def __init__(self):
         self.action_space = gym.spaces.Box(-1.0, 1.0, (21,), np.float64)
-        self.agent = StudentAgent(67,21,self.action_space)
-        self.agent.load('humanoid_sac')
 
     def act(self, observation):
         return self.agent.select_action(observation,evaluate=True)
